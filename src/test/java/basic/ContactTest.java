eb0c7a3bb777c13890d9c1eb437892ab7a491319
AratiGole <aratigole201@gmail.com>
2024-03-23 09:59:43 +0530

    3 class added

--- src/test/java/basic/ContactTest.java
+++ src/test/java/basic/ContactTest.java
@@ -6,19 +6,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class OrganizationsTest {
+public class ContactTest {
 	
 	@Test
-	public void createOrganizations() {
+	public void createContacts() {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://localhost:8888");
 	    driver.findElement(By.name("user_name")).sendKeys("admin");
 	    driver.findElement(By.name("user_password")).sendKeys("admin");
 	    WebElement btn=driver.findElement(By.id("submitButton"));
 	    btn.click();
-	    driver.findElement(By.xpath("//a[text()='Organizations']")).click();
-	   WebElement w= driver.findElements(By.cssSelector("img[alt='Create Organization...']"))
-	    w.click();
+	    driver.findElement(By.xpath("//a[text()='Contacts']")).click();
 	    
 	}
 
